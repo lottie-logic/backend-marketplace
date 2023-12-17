7820af723238f657fba96a5cab97f03e7de4817b
Alex Bondarev <oleksnader.bondarev@gmail.com>
2023-12-17 20:23:33 +0000

    build error fix

--- src/api/middlewares.ts
+++ src/api/middlewares.ts
@@ -15,8 +15,10 @@ const registerLoggedInUser = async (
   if (req.user && req.user.userId) {
     const userService = req.scope.resolve("userService") as UserService;
     loggedInUser = await userService.retrieve(req.user.userId);
+    // @ts-ignore
   } else if (req?.session.user_id) {
     const userService = req.scope.resolve("userService") as UserService;
+     // @ts-ignore
     loggedInUser = await userService.retrieve(req.session.user_id);
   }
 
